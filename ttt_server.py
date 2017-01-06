f2d0d1cbcfd274d84ee00d54644f312c647b9e6a
Kayode Ezike <kezike17@mit.edu>
2017-01-06 09:11:55 -0500

    added 'end' and 'restart' regex

--- ttt_server.py
+++ ttt_server.py
@@ -27,34 +27,56 @@ def ttt_handler():
     })
   board = this_game.board
   turn_rep = this_game.turn_rep
+  # Confirm from user info in request payload
   turn = this_game.rep_to_piece(turn_rep)
-  # Confirm user's turn
-  # TODO
-  # Display board to user
+  ttt_response = ""
   if command == "/ttt":
-    start_match = re.match("^start", command_input)
+    start_match = re.match("start", command_input)
     display_match = re.match("^display$", command_input)
     move_match = re.match("^move [a-z]\d+$", command_input)
+    end_match = re.match("^end$", command_input)
     help_match = re.match("^help$", command_input)
     if start_match:
       # TODO - Check if game already exists for channel
       # If not, set caller's piece to 'x',
       # set opponent's piece to 'o', and display board
       
-      # Configure dimensions of board
-      start_match_flex = re.match("^start ([1-9]|[1-2][0-6])$", command_input)
-      if start_match_flex:
-        (start_cmd, dim) = command_input.split(' ')
-        this_game.board = ttt_rep.TTT_Board(int(dim))
-        board = this_game.board
-      else:
+      # Regex for invoking default-size board (3 x 3)
+      start_and_restart_match = re.match("^(re)?start$", command_input)
+      # Regex for invoking configurable-size board
+      start_and_restart_flex_match = re.match("^(re)?start ([1-9]|[1-2][0-6])$", command_input)
+      if start_and_restart_match:
+        exact_start_match = re.match("^start$", command_input)
+        if exact_start_match:
+          # Check if game is already in progress
+          # TODO - Check this from db
+          if this_game.board != None:
+            return "```Game already in progress! Type '/ttt restart' to start a new game.```"
+        else:
+          # TODO - Update db with flushed board and features and setup new board and features
+          ttt_response = "```Ending current game and starting new game...```"
         # Default to 3 x 3 dimension
         this_game.board = ttt_rep.TTT_Board(3)
         board = this_game.board
-      return board.__str__()
+      elif start_and_restart_flex_match:
+        exact_start_flex_match = re.match("^start ([1-9]|[1-2][0-6])$", command_input)
+        if exact_start_flex_match:
+          # Check if game is already in progress
+          # TODO - Check this from db
+          if this_game.board != None:
+            return "```Game already in progress! Type '/ttt restart' to start a new game.```"
+        else:
+          # TODO - Update db with flushed board and features and setup new board and features
+          ttt_response = "```Ending current game and starting new game with desired configuration...```"
+        # Use configurable board configuration
+        (start_and_restart_cmd, dim) = command_input.split(' ')
+        this_game.board = ttt_rep.TTT_Board(int(dim))
+        board = this_game.board
+      ttt_response += board.__str__()
+      return ttt_response
     elif display_match:
       if board == None:
-        return "```Cannot display board before starting game. Type '/ttt start [DIM]' (where 1 <= DIM <= 26) to play new game.```"
+        return "```Cannot display board before starting game. Type '/ttt start [DIM]' (where 1 <= DIM <= 26) to play a new game.```"
       return board.__str__()
     # Make move
     elif move_match:
@@ -80,6 +102,9 @@ def ttt_handler():
         return "```Position (FILE, RANK) is out of bounds! a <= FILE <= max(a, min(z, MAX_FILE)), where MAX_FILE is the largest lexicographical letter for the board's dimension.```"
       this_game.make_move(fil_str, rnk, turn)
       return board.__str__()
+    elif end_match:
+      # TODO - Update db with flushed board and features
+      return "```Game Ended!```"
     elif help_match:
       manual = open("ttt_manual.txt", 'r')
       return manual.read()
