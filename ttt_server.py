37eb923e5ed8502df3608827df94903c5d4fcc71
Kayode Ezike <kezike17@mit.edu>
2017-01-07 13:30:32 -0500

    testing uid value

--- ttt_server.py
+++ ttt_server.py
@@ -29,6 +29,8 @@ def uname_to_uid(uname):
 
 # Convert Slack user id to username
 def uid_to_uname(uid):
+  print "UID:", uid
+  print "UID TYPE:", type(uid)
   user_info_res = SLACK_CLIENT.api_call("users.info", user=uid)
   if user_info_res["ok"]:
     return user_info_res["user"]["name"]
