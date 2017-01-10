0bdfa112cefdb24df547bcd4667982ba88ebc53b
Kayode Ezike <kezike17@mit.edu>
2017-01-10 10:45:41 -0500

    attempting to address foreign key error

--- ttt_rep.py
+++ ttt_rep.py
@@ -97,7 +97,7 @@ class Board(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   game_id = db.Column(db.Integer, db.ForeignKey("game.id"))
   game = db.relationship("Game", back_populates="board")
-  cells = db.relationship("Cell", order_by="cell.row", back_populates="board")
+  cells = db.relationship("Cell", order_by="Cell.row", back_populates="board")
   turn_rep = db.Column(db.Boolean)
   board_str = db.Column(db.String)
 
