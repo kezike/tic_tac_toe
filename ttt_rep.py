79e14c17c9cbf807ce94e6d199bb378371f3934a
Kayode Ezike <kezike17@mit.edu>
2017-01-11 09:49:52 -0500

    testing

--- ttt_rep.py
+++ ttt_rep.py
@@ -93,7 +93,7 @@ Tic Tac Toe
 class Board(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   # game_id = db.Column(db.Integer, db.ForeignKey("game.id"))
-  game = db.relationship("Game")
+  game = db.relationship("Game", back_populates="board")
   cells = db.relationship("Cell", back_populates="board")
   turn_rep = db.Column(db.Boolean)
   board_str = db.Column(db.String)
